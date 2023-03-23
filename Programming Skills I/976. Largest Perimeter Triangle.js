cd2a07cf941ba5a3c0ad8ac1622752a9e8cce94f
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2023-03-23 23:10:49 +0200

    Create 976. Largest Perimeter Triangle.js

---
+++ Programming Skills I/976. Largest Perimeter Triangle.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const largestPerimeter = (nums)=> {
+    nums.sort( (a,b) => (b - a))
+    let n = nums.length
+    for(let i=0; i<n-2; ++i){
+        if(nums[i] < nums[i+1] + nums[i+2])
+            return (nums[i]+nums[i+1]+nums[i+2])
+    }
+    return 0
+};
